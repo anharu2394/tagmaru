f4dfff8eb6819171f3c2059b6033414e7f807a1d
安藤春香 <haruan2394@gmail.com>
2018-10-21 11:36:26 +0900

    Add: get tag

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'httpclient'
+tags = []
+5.times do |i|
+    client = HTTPClient.new
+    res = client.get("https://qiita.com/api/v2/tags?page=#{i + 1}&per_page=100&sort=count")
+    data = JSON.parse(res.body)
+    p data
+    data.each do |tag|
+        tags << tag["id"]
+    end
+end
+tags.each do |tag|
+    Tag.create(name:tag)
+end
