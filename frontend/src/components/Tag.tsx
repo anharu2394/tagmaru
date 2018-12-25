4a175f7577746713e3a38844465f8b8f39dba23d
安藤春香 <haruan2394@gmail.com>
2018-12-25 21:26:39 +0900

    Add: components

---
+++ frontend/src/components/Tag.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+ 
+class Tag extends React.Component {
+  render() {
+    return (
+      <div>
+          JavaScirpt
+      </div>
+    );
+  }
+}
+ 
+export default Tag;
