31974300b8d7f0860e364c0fff87ba4ff334e2a3
安藤春香 <haruan2394@gmail.com>
2018-12-25 21:08:21 +0900

    Add: devise config on User class

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+            # Include default devise modules.
+       #     devise :database_authenticatable, :registerable,
+        #            :recoverable, :rememberable, :trackable, :validatable,
+         #           :confirmable, :omniauthable, omniauth_providers: [:twitter]
+  devise :rememberable, :omniauthable, omniauth_providers: [:twitter]        
+  include DeviseTokenAuth::Concerns::User
   has_many :follow_tags
   has_many :tags, through: :follow_tags
+
 end
