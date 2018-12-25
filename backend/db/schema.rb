8c5498e4f3d769f1f56bad9dcb961880348265cb
安藤春香 <haruan2394@gmail.com>
2018-12-25 21:14:21 +0900

    Change: schema

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,11 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181019141420) do
+ActiveRecord::Schema.define(version: 20181025104113) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "follow_tags", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "tag_id"
+    t.bigint "user_id"
+    t.bigint "tag_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["tag_id"], name: "index_follow_tags_on_tag_id"
@@ -27,7 +30,7 @@ ActiveRecord::Schema.define(version: 20181019141420) do
     t.integer "fab_count"
     t.date "posted_at"
     t.string "provider"
-    t.integer "tag_id"
+    t.bigint "tag_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["tag_id"], name: "index_posts_on_tag_id"
@@ -40,12 +43,36 @@ ActiveRecord::Schema.define(version: 20181019141420) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "provider"
-    t.string "uid"
-    t.string "username"
-    t.string "image_url"
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.boolean "allow_password_change", default: false
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.string "name"
+    t.string "nickname"
+    t.string "image"
+    t.string "email"
+    t.json "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "follow_tags", "tags"
+  add_foreign_key "follow_tags", "users"
+  add_foreign_key "posts", "tags"
 end
