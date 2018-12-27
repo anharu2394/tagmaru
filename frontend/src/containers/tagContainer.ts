842b7872f0fa0cb132917ba8e05300e4d51b0628
安藤春香 <haruan2394@gmail.com>
2018-12-27 13:11:27 +0900

    Add: tag contaiber connect component

--- frontend/src/containers/tagContainer.ts
+++ frontend/src/containers/tagContainer.ts
@@ -2,4 +2,19 @@ import { Action } from 'typescript-fsa';
 import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
 import { AppState } from '../store';
-import { tagActions } from '../actions/tagActions';
+import { tagActions } from '../actions/tagAction';
+import { TagsComponent } from '../components/TagsComponent'
+
+export interface TagActions {
+}
+
+function mapDispatchToProps() {
+  return {
+  };
+}
+
+function mapStateToProps(appState: AppState) {
+  return Object.assign({}, appState.tags);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TagsComponent);
