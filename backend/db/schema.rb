df4fc2721c32c1766debf11fe1af5736eece819b
Anharu <haruan2394@gmail.com>
2019-05-31 11:00:08 +0900

    Add: save schema that add image column

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181025104113) do
+ActiveRecord::Schema.define(version: 20190531005731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20181025104113) do
     t.bigint "tag_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["tag_id"], name: "index_posts_on_tag_id"
   end
 
